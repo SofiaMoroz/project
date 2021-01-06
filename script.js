d71a704f822bf90aca6c55c888f7fc510795f8a0
Sofia <examle@gmail.com>
2021-01-06 15:18:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+// alert('Hello, world');
+
+// let answer = confirm('Are you here?');
+
+// console.log(answer);
+
+// let answer = +prompt('Есть ли вам 18?', 'Да');
+
+// console.log(typeof(answer));
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+let isCheched = !true,
+    isClose = false;
+
+console.log(isCheched || isClose);
